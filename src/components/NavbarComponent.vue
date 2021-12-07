afc3a5e210c0b41c16e8bd69e930b764ef4523d5
Jaime Nufio <jaimeenufio@gmail.com>
2021-12-06 22:50:06 -0500

    some skeleton work

---
+++ src/components/NavbarComponent.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+      <div>
+        <b-navbar toggleable="lg" type="dark" variant="info">
+          <b-navbar-brand href="#">ADVISR</b-navbar-brand>
+          
+        </b-navbar>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'NavbarComponent',
+  props: {
+  
+  }
+}
+</script>
---
+++ src/components/TableComponent.vue
@@ -0,0 +1,65 @@
+<template>
+  <div>
+
+    <b-container>
+        
+        <b-form @submit="onSubmit" @reset="onReset" v-if="show">
+
+        <b-form-group
+            id="input-group-1"
+            label="Filter By Name:"
+            label-for="input-1"
+
+        >
+            <b-form-input
+            id="input-1"
+            v-model="form.name"
+            type="text"
+            placeholder=""
+            required
+            ></b-form-input>
+              <b-button type="submit" variant="primary">Submit</b-button>
+        </b-form-group>
+        </b-form>
+
+        <b-table striped hover :items="items"></b-table>
+
+    </b-container>
+
+  </div>
+</template>
+
+<script>
+  export default {
+    props:{
+        items: Array
+
+      },
+        data(){
+            return {
+            form: {
+            name: '',
+            },
+            show: true
+        }  
+        },
+        mounted(){
+          
+        },
+    methods: {
+      onSubmit(event) {
+        event.preventDefault()
+        // alert(JSON.stringify(this.form))
+      },
+      
+      fetchBusinesses(which){
+          if (which == -1){
+              //get all
+          }else{
+              //find specifc by id
+          }
+      }
+
+   }
+}
+</script>
